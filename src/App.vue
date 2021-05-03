58f137943d911dd4bd32ddfe918cea3bc6b879e4
JuST-VLaDiK <itworks.by.vlad@gmail.com>
2021-05-03 14:29:58 +0300

    ''

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,377 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <FixWrapper />
+  <div id="slide-1" class="slide ml-83 pt-23.5 h-screen">
+    <div class=" mt-44 relative w-200 h-67.5 services" :class="servicesClass">
+      <div id="first-service" @mouseover="servicesClass='first-state'" @mouseleave="servicesClass='null-state'" class="absolute">
+        <div class="relative flex pt-7.5 pr-7.5 h-full overflow-hidden label">
+          <transition name="service-count">
+            <span v-if="servicesClass !== 'first-state'" class="absolute top-0 left-0">01</span>
+          </transition>
+          <p class="font-medium text-white uppercase ml-0" :class="servicesClass === 'first-state' ? 'ml-100p transform -translate-x-full' : ''">создаём</p>
+          <transition name="service-hover-screen">
+            <div v-if="servicesClass === 'first-state'" class=" absolute top-0 left-0 right-0 bottom-0 hover-screen p-7.5">
+              <div class="flex justify-between">
+                <img  v-if="servicesClass === 'first-state'" src="../src/assets/images/service_icons/loader.svg" class="icon" alt="">
+                <p class="font-medium text-white uppercase">создаём</p>
+              </div>
+              <div class="flex flex-col mt-auto ml-auto items-end">
+                <p><img src="src/assets/images/service_icons/cross.svg" class="w-full ml-4" alt=""></p>
+                <p><img src="src/assets/images/service_icons/cross.svg" class="w-full ml-4" alt=""></p>
+                <p><img src="src/assets/images/service_icons/cross.svg" class="w-full ml-4" alt=""></p>
+              </div>
+            </div>
+          </transition>
+
+        </div>
+      </div>
+      <div id="second-service" @mouseover="servicesClass='second-state'" @mouseout="servicesClass='null-state'" class="absolute">
+        <div class="relative pt-7.5 pr-7.5 overflow-hidden label">
+          <transition name="service-count">
+            <span v-if="servicesClass !== 'second-state'" class="absolute top-0 left-0">02</span>
+          </transition>
+          <p class="font-medium text-white uppercase">ведём</p>
+        </div>
+      </div>
+      <div id="third-service" @mouseover="servicesClass='third-state'" @mouseout="servicesClass='null-state'" class="absolute">
+        <div class="relative pt-7.5 pr-7.5 overflow-hidden label">
+          <transition name="service-count">
+            <span v-if="servicesClass !== 'third-state'" class="absolute top-0 left-0">03</span>
+          </transition>
+          <p class="font-medium text-white uppercase">продвигаем</p>
+        </div>
+      </div>
+      <div id="fourth-service" @mouseover="servicesClass='fourth-state'" @mouseout="servicesClass='null-state'" class="absolute">
+        <div class="relative pt-7.5 pr-7.5 overflow-hidden label">
+          <transition name="service-count">
+            <span v-if="servicesClass !== 'fourth-state'" class="absolute top-0 left-0">04</span>
+          </transition>
+          <p class="font-medium text-white uppercase">пишем</p>
+        </div>
+      </div>
+      <div id="fifth-service" @mouseover="servicesClass='fifth-state'" @mouseout="servicesClass='null-state'" class="absolute">
+        <div class="relative pt-7.5 pr-7.5 overflow-hidden label">
+          <transition name="service-count">
+            <span v-if="servicesClass !== 'fifth-state'" class="absolute top-0 left-0">05</span>
+          </transition>
+          <p class="font-medium text-white uppercase">помогаем</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import FixWrapper from "@/components/FixWrapper";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FixWrapper,
   },
+  data(){
+    return{
+      servicesClass: 'first-state'
+    }
+  }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.services{
+  &>div{
+    transition: all .3s linear;
+    .label {
+      p{
+        font-size: 1.25rem;
+        line-height: 2.5rem;
+        transition: all .3s linear;
+      }
+      span{
+        color:rgba(151, 163, 203, 0.6);
+        font-size: 0.625rem;
+        line-height: 0.625rem;
+      }
+    }
+    .hover-screen{
+      background: rgba(23, 26, 31, 1);
+      z-index: -1;
+    }
+  }
+  //#first-service{
+  //  background: red;
+  //}
+  //#second-service{
+  //  background: blue;
+  //}
+  //#third-service{
+  //  background: green;
+  //}
+  //#fourth-service{
+  //  background: yellow;
+  //}
+  //#fifth-service{
+  //  background: orange;
+  //}
 }
+.services.null-state{
+  &>div{
+    width: 33.3%;
+    height: 50%;
+  }
+  #first-service{
+    top: 0;
+    left: 0;
+  }
+  #second-service{
+    top: 0;
+    left: 33.3%;
+  }
+  #third-service{
+    top: 0;
+    left: 66.6%;
+  }
+  #fourth-service{
+    top: 33.3%;
+    left: 33.3%;
+  }
+  #fifth-service{
+    top: 33.3%;
+    left: 66.6%;
+  }
+}
+
+.services.first-state{
+  &>div{
+    transition: all .3s linear;
+  }
+  #first-service{
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 40%;
+  }
+  #second-service{
+    top: 0;
+    left: 46.6%;
+    height: 33.3%;
+    width: 20%;
+  }
+  #third-service{
+    top: 0;
+    left: 66.6%;
+    height: 33.3%;
+    width: 20%;
+  }
+  #fourth-service{
+    top: 33.3%;
+    left: 46.6%;
+    height: 33.3%;
+    width: 20%;
+  }
+  #fifth-service{
+    top: 33.3%;
+    left: 66.6%;
+    height: 33.3%;
+    width: 20%;
+  }
+  #second-service .label>p, #third-service .label>p,#fourth-service .label>p, #fifth-service .label>p{
+    font-size: 1rem;
+    color:rgba(151, 163, 203, 0.6);
+  }
+}
+
+.services.second-state{
+  &>div{
+    transition: all .3s linear;
+
+  }
+  #first-service{
+    top: 0;
+    left: 0;
+    height: 33.3%;
+    width: 20%;
+  }
+  #second-service{
+    top: 0;
+    left: 20%;
+    height: 100%;
+    width: 40%;
+
+  }
+  #third-service{
+    top: 0;
+    left: 66.6%;
+    height: 33.3%;
+    width: 20%;
+  }
+  #fourth-service{
+    top: 33.3%;
+    left: 66.6%;
+    height: 33.3%;
+    width: 20%;
+  }
+  #fifth-service{
+    top: 66.6%;
+    left: 66.6%;
+    height: 33.3%;
+    width: 20%;
+  }
+  #first-service .label>p, #third-service .label>p,#fourth-service .label>p, #fifth-service .label>p{
+    font-size: 1rem;
+    color:rgba(151, 163, 203, 0.6);
+  }
+}
+
+.services.third-state{
+  &>div{
+    transition: all .3s linear;
+  }
+  #first-service{
+    top: 0;
+    left: 0;
+    height: 33.3%;
+    width: 25%;
+  }
+  #second-service{
+    top: 0;
+    left: 25%;
+    height: 33.3%;
+    width: 25%;
+
+  }
+  #third-service{
+    top: 0;
+    left: 56.6%;
+    height: 100%;
+    width: 40%;
+  }
+  #fourth-service{
+    top: 33.3%;
+    left: 0%;
+    height: 33.3%;
+    width: 25%;
+  }
+  #fifth-service{
+    top: 33.3%;
+    left: 25%;
+    height: 33.3%;
+    width: 25%;
+  }
+  #second-service .label>p, #first-service .label>p,#fourth-service .label>p, #fifth-service .label>p{
+    font-size: 1rem;
+    color:rgba(151, 163, 203, 0.6);
+  }
+}
+
+.services.fourth-state{
+  &>div{
+    transition: all .3s linear;
+  }
+  #first-service{
+    top: 0;
+    left: 0;
+    height: 33.3%;
+    width: 20%;
+  }
+  #second-service{
+    top: 33.3%;
+    left: 0%;
+    height: 33.3%;
+    width: 20%;
+  }
+  #third-service{
+    top: 0;
+    left: 66.6%;
+    height: 33.3%;
+    width: 20%;
+  }
+  #fourth-service{
+    top: 0%;
+    left: 20%;
+    height: 100%;
+    width: 40%;
+  }
+  #fifth-service{
+    top: 33.3%;
+    left: 66.6%;
+    height: 33.3%;
+    width: 20%;
+  }
+  #second-service .label>p, #third-service .label>p,#first-service .label>p, #fifth-service .label>p{
+    font-size: 1rem;
+    color:rgba(151, 163, 203, 0.6);
+  }
+}
+
+.services.fifth-state{
+  &>div{
+    transition: all .3s linear;
+  }
+  #first-service{
+    top: 0;
+    left: 0;
+    height: 33.3%;
+    width: 25%;
+  }
+  #second-service{
+    top: 0;
+    left: 25%;
+    height: 33.3%;
+    width: 25%;
+
+  }
+  #third-service{
+    top: 33.3%;
+    left: 25%;
+    height: 33.3%;
+    width: 25%;
+  }
+  #fourth-service{
+    top: 33.3%;
+    left: 0%;
+    height: 33.3%;
+    width: 25%;
+  }
+  #fifth-service{
+    top: 0;
+    left: 56.6%;
+    height: 100%;
+    width: 40%;
+  }
+  #second-service .label>p, #third-service .label>p,#fourth-service .label>p, #first-service .label>p{
+    font-size: 1rem;
+    color:rgba(151, 163, 203, 0.6);
+  }
+}
+
+
+.service-count-enter-active,
+.service-count-leave-active,
+.service-hover-screen-enter-active,
+.service-hover-screen-leave-active,{
+  transition: all .3s linear;
+  .icon{
+    transition: all .3s linear;
+  }
+}
+
+.service-count-enter-from,
+.service-count-leave-to {
+  transform: translateX(300%);
+  opacity: 0;
+}
+
+.service-hover-screen-enter-from,
+.service-hover-screen-leave-to {
+  transform: translateX(-100%);
+  opacity: 0;
+  .icon{
+    transform:translateY(-9%)  rotate(180deg) ;
+  }
+}
+
+
+
+
+
 </style>
---
+++ src/components/FixWrapper.vue
@@ -0,0 +1,361 @@
+<template>
+  <WrapGrads />
+  <div class="slider-wrapper-top fixed flex left-18 right-18 top-11 pt-">
+    <div class="menu cursor-pointer w-12.5">
+      <div class="menu-content">
+        <p class="uppercase transition-all font-medium text-xs mb-3">menu</p>
+        <div class="h-5 relative w-full">
+          <div
+            class="absolute top-0 h-2px w-full bg-white transition-all"
+          ></div>
+          <div
+            id="second-menu-strip"
+            class="absolute bottom-0 h-2px w-1/2 bg-white transition-all"
+          ></div>
+        </div>
+      </div>
+      <div class="menu-grad relative -top-32 left-0" style="z-index: -1">
+        <div class="blue-b"></div>
+        <div class="blue-s"></div>
+        <div class="purple"></div>
+        <div class="purple-s"></div>
+      </div>
+    </div>
+    <div class="max-frai ml-52.5">
+      <img src="@/assets/images/max-frai.svg" class="w-full" alt="" />
+      <p
+        class="uppercase inter font-bold mt-4"
+        style="color: #ff2e53; letter-spacing: 1rem; line-height: 1"
+      >
+        AGENCY
+      </p>
+    </div>
+    <div class="ml-27 h-full mt-auto">
+      <div class="h-8px w-px" style="background: #ff2e53"></div>
+    </div>
+    <div class="ml-65 agency">
+      <p class="font-medium text-base">
+        Агентство простых решений сложного <br />
+        цифрового мира
+      </p>
+    </div>
+    <div class="connect ml-auto">
+      <div class="connect-content flex items-center">
+        <p class="uppercase font-semibold text-xsm mr-4">СВЯЗАТЬСЯ С НАМИ</p>
+        <div class="plus w-12.5 h-12.5 rounded-full relative">
+          <div
+            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4 h-2px"
+          ></div>
+          <div
+            class="absolute top-1/2 left-1/2 transform rotate-90 -translate-x-1/2 -translate-y-1/2 w-4 h-2px"
+          ></div>
+        </div>
+      </div>
+
+      <div class="connect-grad relative" style="z-index: -1">
+        <div id="grad-1"></div>
+        <div id="grad-2"></div>
+        <div id="grad-3"></div>
+      </div>
+    </div>
+  </div>
+  <div class="slider-wrapper-left fixed bottom-18 left-18 top-50">
+    <div class="relative w-22.5 h-full first-state states">
+      <div id="first-strip"></div>
+      <div id="second-strip"></div>
+      <div id="third-strip"></div>
+      <div id="fourth-strip"></div>
+      <div id="fifth-strip"></div>
+      <div id="sixth-strip"></div>
+      <div id="seventh-strip"></div>
+      <div class="absolute left-0 h-px w-1/3 opacity-30 bottom-0"></div>
+    </div>
+  </div>
+  <div class="slider-wrapper-right fixed bottom-18 right-18 top-50">
+    <div class="relative w-22.5 h-full">
+      <div
+        class="absolute right-0 transition-all h-px w-1/3 opacity-30"
+        style="top: 0"
+      ></div>
+      <div
+        class="absolute right-0 transition-all h-3px w-1/6 opacity-60"
+        style="top: 41.34897%"
+      ></div>
+      <div
+        class="absolute right-0 transition-all h-3px w-1/15 opacity-30"
+        style="top: 43.69501466%"
+      ></div>
+      <div
+        class="absolute flex bottom-0 right-0"
+        style="background: transparent"
+      >
+        <div class="numbers flex mr-2">
+          <p class="text-3xl" style="line-height: 0.78">1</p>
+        </div>
+        <div class="flex flex-col">
+          <div>
+            <p class="number text-xxs">7</p>
+          </div>
+          <div
+            class="h-px w-15px mt-1"
+            style="background: rgba(151, 163, 203, 0.6)"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import WrapGrads from "@/components/WrapGrads";
+
+export default {
+  name: "wrappers",
+  components: { WrapGrads },
+  data() {
+    return {
+      menuHover: null,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.slider-wrapper-left {
+  .states {
+    div {
+      transition: all 0.2s linear;
+      position: absolute;
+      left: 0;
+      background: #97a3cb;
+    }
+  }
+  .first-state {
+    #first-strip {
+      height: 1px;
+      width: 33%;
+      opacity: 0.3;
+      top: 0;
+    }
+    #second-strip {
+      height: 1px;
+      width: 100%;
+      opacity: 0.3;
+      top: 10.41055805%;
+    }
+    #third-strip {
+      height: 3px;
+      width: 11.111%;
+      opacity: 0.6;
+      top: 33.5777126%;
+    }
+    #fourth-strip {
+      height: 1px;
+      width: 22.222%;
+      opacity: 0.3;
+      top: 37.683284%;
+    }
+    #fifth-strip {
+      height: 3px;
+      width: 11.111%;
+      opacity: 0.6;
+      top: 41.4956027%;
+    }
+    #sixth-strip {
+      height: 3px;
+      width: 11.111%;
+      opacity: 0.3;
+      top: 44.8680351%;
+    }
+    #seventh-strip {
+      height: 1px;
+      width: 11.111%;
+      opacity: 0.3;
+      top: 48.2404692%;
+    }
+  }
+}
+.slider-wrapper-left,
+.slider-wrapper-right {
+  .absolute {
+    background: #97a3cb;
+  }
+}
+.slider-wrapper-top {
+  .menu {
+    .menu-grad {
+      div {
+        border-radius: 50%;
+        transform: scale(0);
+        transition: all 0.15s linear;
+      }
+      .blue-b {
+        position: absolute;
+        width: 180.46px;
+        height: 180.46px;
+        left: -104px;
+        top: 18px;
+
+        background: linear-gradient(
+          321.88deg,
+          rgba(7, 25, 61, 0) 12.53%,
+          #f22959 43.45%,
+          #0085ff 60.5%
+        );
+        filter: blur(60px);
+      }
+      .blue-s {
+        position: absolute;
+        width: 112.05px;
+        height: 112.05px;
+        left: 44px;
+        top: 22px;
+
+        background: linear-gradient(
+          209.3deg,
+          rgba(0, 133, 255, 0) -2.55%,
+          #0085ff 64.59%
+        );
+        filter: blur(60px);
+        transform: rotate(165deg) scale(0);
+      }
+      .purple {
+        position: absolute;
+        width: 180.46px;
+        height: 180.46px;
+        left: -34px;
+        top: 86px;
+
+        background: linear-gradient(
+          209.3deg,
+          rgba(242, 137, 41, 0) -2.55%,
+          #f22959 64.59%
+        );
+        opacity: 0.7;
+        filter: blur(60px);
+        transform: rotate(165deg) scale(0);
+      }
+      .purple-s {
+        position: absolute;
+        width: 180.46px;
+        height: 180.46px;
+        left: -70px;
+        top: 47px;
+
+        background: linear-gradient(
+          321.88deg,
+          rgba(7, 25, 61, 0) 12.53%,
+          #f22959 43.45%,
+          #0085ff 60.5%
+        );
+        filter: blur(60px);
+      }
+    }
+    .menu-content:hover {
+      p {
+        letter-spacing: 0.3rem;
+      }
+      #second-menu-strip {
+        width: 100%;
+      }
+    }
+    .menu-content:hover + .menu-grad {
+      div {
+        transform: scale(1);
+      }
+      .blue-s {
+        transform: rotate(165deg) scale(1);
+      }
+      .purple {
+        transform: rotate(165deg) scale(1);
+      }
+    }
+  }
+
+  .connect {
+    p,
+    .plus,
+    .plus div {
+      transition: all 0.2s linear;
+    }
+    .plus {
+      background: #ff2e53;
+      div {
+        background: white;
+      }
+    }
+    .connect-grad {
+      left: 3rem;
+      top: -3rem;
+      div {
+        border-radius: 50%;
+        transform: scale(0);
+        transition: all 0.15s linear;
+      }
+      #grad-1 {
+        position: absolute;
+        width: 180.46px;
+        height: 180.46px;
+        left: 66px;
+        top: -90px;
+
+        background: linear-gradient(
+          321.88deg,
+          rgba(7, 25, 61, 0) 12.53%,
+          #f22959 43.45%,
+          #ff0000 60.5%
+        );
+        filter: blur(60px);
+      }
+      #grad-2 {
+        position: absolute;
+        width: 112.05px;
+        height: 112.05px;
+        left: 203.59px;
+        top: -86.41px;
+        transform: rotate(165deg) scale(0);
+        background: linear-gradient(209.3deg, #7b61ff -2.55%, #7b61ff 64.59%);
+        filter: blur(60px);
+      }
+      #grad-3 {
+        position: absolute;
+        width: 180.46px;
+        height: 180.46px;
+        left: 136.28px;
+        top: -21.72px;
+        transform: rotate(165deg) scale(0);
+        background: linear-gradient(
+          209.3deg,
+          rgba(242, 137, 41, 0) -2.55%,
+          #f22959 64.59%
+        );
+        opacity: 0.7;
+        filter: blur(60px);
+      }
+    }
+    .connect-content:hover {
+      p {
+        margin-right: 3rem;
+      }
+      .plus {
+        transform: rotate(180deg);
+        background: white;
+        div {
+          background: black;
+        }
+      }
+    }
+    .connect-content:hover + .connect-grad {
+      div {
+        transform: scale(1);
+      }
+      #grad-2 {
+        transform: rotate(165deg) scale(1);
+      }
+      #grad-3 {
+        transform: rotate(165deg) scale(1);
+      }
+    }
+  }
+}
+</style>
