de5681816102d18777d87e569726ab248bbbc949
JuST-VLaDiK <itworks.by.vlad@gmail.com>
2021-05-16 14:43:21 +0300

    ''

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="body" @scroll.stop="" :class="'slide-'+slide">
+  <div class="body" @scroll.prevent.self="" :class="'slide-'+slide">
     <FixWrapper @nextSlide="nextSlide()" @prevSlide="prevSlide()" :state="slide" />
     <div id="slide-1" class="slide ml-83 pt-23.5 h-screen relative">
       <servicesScreen />
