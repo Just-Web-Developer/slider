563e1b863bd0a1355072dbcb088bff36a6980efd
JuST-VLaDiK <itworks.by.vlad@gmail.com>
2021-05-16 12:47:10 +0300

    ''

--- src/components/firstAnimation.vue
+++ src/components/firstAnimation.vue
@@ -1,6 +1,6 @@
 <template>
   <div  class="first-slide-animation">
-    <div id="hud_1" class="flex justify-end items-center  hud relative" style="transform: rotate(-7deg)" >
+    <div id="hud_1" class="flex justify-end items-center  hud relative" style="transform: rotate(11deg)" >
       <img  src="../assets/images/animation_hud/1.svg" class="absolute" style="transform: rotate(-1.3deg); width: 5.5%;" alt="">
     </div>
     <div id="hud_2" class="flex justify-end items-center  hud absolute" style="transform: rotate(56deg)" >
@@ -40,49 +40,142 @@ export default {
     }
   }
 
-  //#hud_1{
-  //  animation:linear 7s infinite normal hud_1;
-  //}
-  //@keyframes hud_1{
-  //  0%{
-  //    transform: rotate(-7deg);
-  //  }
-  //  100%{
-  //    transform: rotate(-353deg);
-  //  }
-  //}
-  //#hud_2{
-  //  animation:linear 7s infinite normal hud_2;
-  //}
-  //@keyframes hud_2{
-  //  0%{
-  //    transform: rotate(56deg);
-  //  }
-  //  100%{
-  //    transform: rotate(-304deg);
-  //  }
-  //}
-  //#hud_3{
-  //  animation:linear 7s infinite normal hud_3;
-  //}
-  //@keyframes hud_3{
-  //  0%{
-  //    transform: rotate(45deg);
-  //  }
-  //  100%{
-  //    transform: rotate(-315deg);
-  //  }
-  //}
-  //#hud_4{
-  //  animation:linear 7s infinite normal hud_4;
-  //}
-  //@keyframes hud_4{
-  //  0%{
-  //      transform: rotate(-118deg);
-  //  }
-  //  100%{
-  //      transform: rotate(-478deg);
-  //  }
-  //}
+  #hud_1{
+    animation:linear 7s infinite normal hud_1;
+  }
+  @keyframes hud_1{
+    0%{
+      transform: rotate(11deg);
+    }
+    15%{
+      transform: rotate(11deg);
+    }
+    35%{
+      transform: rotate(11deg);
+    }
+    40%{
+      transform: rotate(-70deg);
+    }
+    55%{
+      transform: rotate(-70deg);
+    }
+    60%{
+      transform: rotate(-220deg);
+    }
+    75%{
+      transform: rotate(-220deg);
+    }
+    80%{
+      transform: rotate(-300deg);
+    }
+    95%{
+      transform: rotate(-300deg);
+    }
+    100%{
+      transform: rotate(-349deg);
+    }
+  }
+  #hud_2{
+    animation:linear 7s infinite normal hud_2;
+  }
+  @keyframes hud_2{
+    0%{
+      transform: rotate(56deg);
+    }
+    15%{
+      transform: rotate(56deg);
+    }
+    35%{
+      transform: rotate(56deg);
+    }
+    40%{
+      transform: rotate(-25deg);
+    }
+    55%{
+      transform: rotate(-25deg);
+    }
+    75%{
+      transform: rotate(-25deg);
+    }
+    80%{
+      transform: rotate(-105deg);
+    }
+    95%{
+      transform: rotate(-105deg);
+    }
+
+    100%{
+      transform: rotate(-304deg);
+    }
+  }
+  #hud_3{
+    animation:linear 7s infinite normal hud_3;
+  }
+  @keyframes hud_3{
+    0%{
+      transform: rotate(45deg);
+    }
+    15%{
+      transform: rotate(45deg);
+    }
+    35%{
+      transform: rotate(45deg);
+    }
+    40%{
+      transform: rotate(-36deg);
+    }
+    55%{
+      transform: rotate(-36deg);
+    }
+    75%{
+      transform: rotate(-36deg);
+    }
+    80%{
+      transform: rotate(-116deg);
+    }
+    95%{
+      transform: rotate(-116deg);
+    }
+
+    100%{
+      transform: rotate(-315deg);
+    }
+  }
+  #hud_4{
+    animation:linear 7s infinite normal hud_4;
+  }
+  @keyframes hud_4{
+    0%{
+      transform: rotate(-118deg);
+    }
+    15%{
+      transform: rotate(-118deg);
+    }
+    20%{
+      transform: rotate(-282deg);
+    }
+    35%{
+      transform: rotate(-282deg);
+    }
+    40%{
+      transform: rotate(-363deg);
+    }
+    55%{
+      transform: rotate(-363deg);
+    }
+    75%{
+      transform: rotate(-363deg);
+    }
+    80%{
+      transform: rotate(-443deg);
+    }
+    95%{
+      transform: rotate(-443deg);
+    }
+
+    100%{
+        transform: rotate(-478deg);
+    }
+  }
 
 </style>
