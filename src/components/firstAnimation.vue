87b780d26128aea27aec3942bfca0526f33103d1
JuST-VLaDiK <itworks.by.vlad@gmail.com>
2021-05-16 11:12:30 +0300

    ''

--- src/components/firstAnimation.vue
+++ src/components/firstAnimation.vue
@@ -1,17 +1,17 @@
 <template>
   <div  class="first-slide-animation">
-<!--    <div id="hud_1" class="flex justify-end items-center  hud relative" style="transform: rotate(45deg)" >-->
-<!--      <img  src="../assets/images/animation_hud/1.svg" class="absolute" style="transform: rotate(45deg)" alt="">-->
-<!--    </div>-->
-<!--    <div id="hud_2" class="flex justify-end items-center  hud absolute" >-->
-<!--      <img  src="../assets/images/animation_hud/2.svg" style="transform: rotate(-70deg);position: relative;right: -29px; " alt="">-->
-<!--    </div>-->
-<!--    <div id="hud_3" class="flex justify-end items-center  hud absolute" >-->
-<!--      <img src="../assets/images/animation_hud/3.svg" style="position: relative;right: 2px;transform: rotate(-58deg);" alt="">-->
-<!--    </div>-->
-<!--    <div id="hud_4" class="flex justify-end items-center  hud absolute" >-->
-<!--      <img  src="../assets/images/animation_hud/4.svg" style="transform: rotate(-5deg);" alt="">-->
-<!--    </div>-->
+    <div id="hud_1" class="flex justify-end items-center  hud relative" style="transform: rotate(45deg)" >
+      <img  src="../assets/images/animation_hud/1.svg" class="absolute" style="transform: rotate(45deg)" alt="">
+    </div>
+    <div id="hud_2" class="flex justify-end items-center  hud absolute" >
+      <img  src="../assets/images/animation_hud/2.svg" style="transform: rotate(-70deg);position: relative;right: -29px; " alt="">
+    </div>
+    <div id="hud_3" class="flex justify-end items-center  hud absolute" >
+      <img src="../assets/images/animation_hud/3.svg" style="position: relative;right: 2px;transform: rotate(-58deg);" alt="">
+    </div>
+    <div id="hud_4" class="flex justify-end items-center  hud absolute" >
+      <img  src="../assets/images/animation_hud/4.svg" style="transform: rotate(-5deg);" alt="">
+    </div>
     <video
       class="absolute w-240 right-0 top-0 first-slide-animation"
       style="z-index: -10"
