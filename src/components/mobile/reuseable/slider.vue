af3bdaa5a99cb3e028e1d29ecd3f77a2193eaf41
Vladyslav Smolianets <vsmolianets@stalker2.com>
2021-11-12 01:00:01 +0200

    Slider tests

--- src/components/mobile/reuseable/slider.vue
+++ src/components/mobile/reuseable/slider.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="mobile-slider" @mousemove="move($event)" class="slider ml-4.5 flex w-max max-w-none left-0 relative min-h-42.5 mb-40">
+  <p class="text-red-700">{{touch.start}}</p>
+  <p class="text-yellow-600">{{touch.current}}</p>
+  <p class="text-green-600">{{touch.end}}</p>
+  <div id="mobile-slider" class="slider ml-4.5 flex w-max max-w-none left-0 relative min-h-42.5 mb-40" :style="'left:' + swipe + 'px' ">
     <div class="slide mr-7.5 w-62.5 flex flex-col">
       <img src="@/assets/images/slide_2/stack/enter.svg"
            class="h-19 w-19 p-4.5 rounded-full mb-5"
@@ -34,16 +37,49 @@
 <script>
 export default {
   name: "slider",
+  data(){
+    return{
+      touch:{
+        start:0,
+        prevStart:0,
+        current:0,
+        prevCurrent:0,
+        end:0,
+        prevEnd:0,
+        cancel:0
+      }
+    }
+  },
+  computed:{
+    swipe(){
+      if (this.touch.current === 0 || this.touch.current === this.touch.prevCurrent ){
+        return 0
+      }
+      this.setPrev('Current')
+      let res = (this.touch.current - this.touch.start)
+      console.log(res)
+      return res
+    }
+  },
   methods:{
-    start(e){
-      console.log(e);
-    },
-    move(e){
-      console.log(e);
-    },
-    end(){
-      console.log();
-    },
+    setPrev(e){
+      this.touch['prev' + e] = this.touch[e.toLowerCase()]
+    }
+  },
+  mounted() {
+    document.getElementById('mobile-slider').addEventListener('touchstart', (event)=>{
+      this.touch.start = event.changedTouches[0].clientX
+      console.log(event.changedTouches[0].clientX)
+    })
+    document.getElementById('mobile-slider').addEventListener('touchmove', (event)=>{
+      this.touch.current = event.changedTouches[0].clientX
+    })
+    document.getElementById('mobile-slider').addEventListener('touchend', (event)=>{
+      this.touch.end = event.changedTouches[0].clientX
+    })
+    document.getElementById('mobile-slider').addEventListener('touchcancel', (event)=>{
+      this.touch.cancel = event.changedTouches[0].clientX
+    })
   }
 };
 </script>
